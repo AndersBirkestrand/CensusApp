ce4c98c3782d80988dd9422305ead4879427450e
AndersBirkestrand <164764890+AndersBirkestrand@users.noreply.github.com>
2025-05-09 11:26:24 +0200

    Issue with seq.sync, trying something new.

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 const participantsRouter = require("./routes/participants");
 
-db.sequelize.sync({ alter: true });
+db.sequelize.sync();
 var app = express();
 
 // view engine setup
